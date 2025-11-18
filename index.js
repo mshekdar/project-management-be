f4ee7171b8af98f1bd8d10a1e28ab3fca9782404
Mayuresh Shekdar <shekdarm@gmail.com>
2025-11-18 09:58:17 +0000

    Add dotenv & set up .env

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Start of backend project");
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"
+})
+
+const myUsername = process.env.username
+const database = process.env.database
+
+console.log("Username :", myUsername);
+console.log("Database :", database);
